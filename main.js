65f54ed51d152b1117a8b7342e657f111e881923
IamChplayz <165848128+IamChplayz@users.noreply.github.com>
2024-09-13 12:16:33 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
 console.log("main.js loaded");
 
-const axios = require('axios');
 
-axios.get('https://www.roblox.com/home/api', {
-  headers: { 'Cookie': '.ROBLOSECURITY=yourCookieValue' }
-})
-  .then(response => {
-    // Access cookies and other headers here
-    const cookies = response.headers['set-cookie'];
-    console.log(cookies);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
